e6fc29decb535e12cf4b827ffd6f71077f453ea7
edlg01 <91291047+edlg01@users.noreply.github.com>
2025-05-06 07:41:15 -0600

    Add files via upload

---
+++ calculadora-resta/src/main/java/com/ejemplo/resta/RestaApplication.java
@@ -0,0 +1,11 @@
+package com.ejemplo.resta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestaApplication.class, args);
+    }
+}
